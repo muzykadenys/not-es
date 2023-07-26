f1c39502e23f14ca0dd4552fa19c8e19b47c1f1d
muzykadenys <denysmuzyka@gmail.com>
2023-07-26 14:59:41 +0300

    change start load

--- src/hooks/useGetDataFromStore.ts
+++ src/hooks/useGetDataFromStore.ts
@@ -12,8 +12,10 @@ function useGetDataFromStore() {
     try {
       const data: string | null = localStorage.getItem("cardList");
 
+      console.log(data);
       if (data)
         dispatch({ type: CARDINFO_GET_SUCCESS, payload: JSON.parse(data) });
+      else dispatch({ type: CARDINFO_GET_SUCCESS, payload: [] });
     } catch (e) {
       dispatch({ type: CARDINFO_GET_ERROR, error: e });
     }
